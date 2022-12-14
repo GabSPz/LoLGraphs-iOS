7d682fe81854c2ef4f82c5a9714a7e232343aec8
Gabriel Peraza <gabrielsanchezperaza@gmail.com>
2022-12-13 22:07:08 -0600

    Initial Commit

---
+++ LoLGraphs-iOS/LoLGraphs_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoLGraphs_iOSApp.swift
+//  LoLGraphs-iOS
+//
+//  Created by Gabriel Sanchez Peraza on 13/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LoLGraphs_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
